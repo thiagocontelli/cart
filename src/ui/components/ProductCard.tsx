99ea6d021a399d2b36534c9d067c1a99d65541f7
thiagocontelli <thiagocontellid@gmail.com>
2023-01-25 23:11:10 -0300

    feat: add some improvements to ProductCard ui

--- src/ui/components/ProductCard.tsx
+++ src/ui/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardMedia } from '@mui/material'
+import { Box, Card, CardMedia } from '@mui/material'
 import Button from '@mui/material/Button'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
@@ -17,21 +17,31 @@ interface ProductCardProps {
 
 export function ProductCard ({ brand, price, rating, title, thumbnail, onClick }: ProductCardProps) {
   return (
-    <Card sx={{ maxWidth: '300px' }}>
-      <CardMedia
-        sx={{ maxHeight: '200px', objectFit: 'contain' }}
-        component='img'
-        image={thumbnail}
-      />
-      <CardContent>
-        <Typography variant='h4'>{title}</Typography>
-        <Typography variant='subtitle1'>{brand}</Typography>
-        <Typography variant='h5' marginTop='1rem'>{currencyConverter(price)}</Typography>
-        <Typography marginTop='1rem' sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
-          <Star size={20} color="#d4af37" weight="fill" />
-          {rating.toFixed(2)}
-        </Typography>
-      </CardContent>
+    <Card
+      sx={{
+        flex: 1,
+        minHeight: '500px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexDirection: 'column'
+      }}
+    >
+      <Box>
+        <CardMedia
+          sx={{ maxHeight: '200px', objectFit: 'cover' }}
+          component='img'
+          image={thumbnail}
+        />
+        <CardContent>
+          <Typography title={title} variant='h5' noWrap>{title}</Typography>
+          <Typography variant='subtitle1'>{brand}</Typography>
+          <Typography variant='h5' marginTop='1rem'>{currencyConverter(price)}</Typography>
+          <Typography marginTop='1rem' sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+            <Star size={20} color="#d4af37" weight="fill" />
+            {rating.toFixed(2)}
+          </Typography>
+        </CardContent>
+      </Box>
       <CardActions>
         <Button
           variant='contained'
