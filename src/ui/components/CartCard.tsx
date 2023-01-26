ce5f70f19d23634746e4b1caab96d55455412d3b
thiagocontelli <thiagocontellid@gmail.com>
2023-01-25 21:36:05 -0300

    feat: create CartCard component

---
+++ src/ui/components/CartCard.tsx
@@ -0,0 +1,33 @@
+import { Avatar, Card, CardHeader, IconButton, Typography } from '@mui/material'
+import { Minus, Plus } from 'phosphor-react'
+import { Product } from '../../model/Product'
+import { currencyConverter } from '../helper/currencyConverter'
+
+interface CartCardProps {
+  product: Product
+}
+
+export function CartCard ({ product }: CartCardProps) {
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar src={product.thumbnail} />
+        }
+        title={product.title}
+        subheader={currencyConverter(product.price)}
+        action={
+          <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+            <IconButton>
+              <Minus size={20} color="#000000" weight="fill" />
+            </IconButton>
+            <Typography>10</Typography>
+            <IconButton>
+              <Plus size={20} color="#000000" weight="fill" />
+            </IconButton>
+          </div>
+        }
+      />
+    </Card>
+  )
+}
