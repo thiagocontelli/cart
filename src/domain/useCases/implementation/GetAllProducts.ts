2de9edcc7e40f8dc6dd0f5082e26bcde81c8a40a
thiagocontelli <thiagocontellid@gmail.com>
2023-01-25 22:33:45 -0300

    feat: add a response dto to GetAllProductsUseCase

--- src/domain/useCases/implementation/GetAllProducts.ts
+++ src/domain/useCases/implementation/GetAllProducts.ts
@@ -2,6 +2,20 @@ import { Product } from '../../../model/Product'
 import { IGetAllProductsRepository } from '../../interfaces/IGetAllProductsRepository'
 import { IGetAllProducts } from '../abstraction/IGetAllProducts'
 
+interface ResponseDto {
+  id: number
+  title: string
+  description: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  brand: string
+  category: string
+  thumbnail: string
+  images: string[]
+}
+
 export class GetAllProducts implements IGetAllProducts {
   constructor (
     private readonly repository: IGetAllProductsRepository
@@ -10,7 +24,9 @@ export class GetAllProducts implements IGetAllProducts {
   async execute (): Promise<Product[]> {
     try {
       const { data } = await this.repository.getAllProducts()
-      return data.products
+      return data.products.map((it: ResponseDto) => (
+        new Product(it.id, it.title, it.price, it.rating, it.brand, it.thumbnail)
+      ))
     } catch (error) {
       throw new Error()
     }
